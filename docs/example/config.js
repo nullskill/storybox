217059c658749e15ed6302271a7018ee876d56d7
nullskill <2liv@mail.ru>
2017-06-25 10:42:50 +0300

    Update config.js
    React, react-router4 & styled-components support

--- docs/example/config.js
+++ docs/example/config.js
@@ -1,3 +1,15 @@
-import { config } from 'storybox';
+import React from 'react';
+import { config, configure, addDecorator } from 'storybox';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from 'components';
 import params from './params';
+
+addDecorator(story => (
+  <BrowserRouter>
+     <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+  </BrowserRouter>
+))
+
 config(params);
