de517f2029b1c26618cf5cfd6acff8eda8580490
nullskill <2liv@mail.ru>
2017-06-26 11:06:15 +0300

    Update config.js
    w redux store

--- docs/example/config.js
+++ docs/example/config.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { config, configure, addDecorator } from 'storybox';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
+import reducers from 'store/reducers';
 import { theme } from 'components';
 import params from './params';
 
+// PASSING INITIAL STATE FROM SERVER STORE
+const initialState = window.INITIAL_STATE;
+const store = createStore(reducers, initialState);
+
 addDecorator(story => (
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
      <ThemeProvider theme={theme}>{story()}</ThemeProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 ))
 
 config(params);
