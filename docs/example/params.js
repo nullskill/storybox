3ffde93e105f872737063d89ef98d1cee945c36e
nullskill <2liv@mail.ru>
2017-07-09 17:10:22 +0300

    Update params.js

--- docs/example/params.js
+++ docs/example/params.js
@@ -1,7 +1,26 @@
-export default {
-  modules: require('glob-loader!./glob.txt'),
+// export default {
+//   modules: require('glob-loader!./glob.txt'),
+//   options: {
+//     name: 'YourComponent',
+//   },
+//   info: true,
+// };
+
+// const path = require('path');
+// const context = require.context('components', true, /.story.js$/)
+
+let stories = {
+  modules: {},
   options: {
     name: 'YourComponent',
   },
   info: true,
+  isomorphicStyles: false
 };
+
+// context.keys().forEach(filename => {
+//   let module = context(filename);
+//   stories.modules[path.join('../src/components/', filename)] = module;
+// });
+
+export default stories;
